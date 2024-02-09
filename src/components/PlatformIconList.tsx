90fe7404ff0a06eecb91865bde0a3327462735a0
Emiliya Dobrogorska <emiliyadobrogorska@Emiliyas-MacBook-Pro-2.local>
2024-02-08 21:25:03 -0800

    filtering games by genre

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -37,7 +37,11 @@ const PlatformIconList = ({ platforms }: Props) => {
       <HStack marginY={1}>
         {platforms.map((platform) => (
           // <Text>{platform.name}</Text>
-          <Icon as={iconMap[platform.slug]} color="gray.500" />
+          <Icon
+            as={iconMap[platform.slug]}
+            color="gray.500"
+            key={platform.id}
+          />
         ))}
       </HStack>
     </>

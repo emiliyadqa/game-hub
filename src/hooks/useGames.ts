feca0862fef802bd22a76ce9cf4aa694bd8c27b1
Emiliya Dobrogorska <emiliyadobrogorska@Emiliyas-MacBook-Pro-2.local>
2024-03-07 22:11:42 -0800

    Implement infinite query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,7 @@
 // import { useEffect, useState } from "react";
 // import apiClient from "../services/api-client";
 // import { CanceledError } from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 // import useData from "./useData";
 // import apiClient from "../services/api-client";
@@ -24,17 +24,22 @@ const useGames = (
   // selectedGenre: Genre | null,
   // selectedPlatform: Platform | null
 ) =>
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
+          page: pageParam,
         },
       }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+    initialPageParam: 1,
     // queryFn: () =>
     //   apiClient
     //     .get<FetchResponse<Game>>("/games", {
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -5,7 +5,6 @@ import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 // import apiClient from "../services/api-client";
 import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
 
 const apiClient = new APIClient<Genre>("/genres");
 

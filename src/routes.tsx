144040bcc244c7be202116d96f4aa7f19bf0cdd4
Emiliya Dobrogorska <emiliyadobrogorska@Emiliyas-MacBook-Pro-2.local>
2024-03-25 23:18:22 -0700

    Handling Errors with routerError hook

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import GameDetaiPage from "./pages/GameDetaiPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:is", element: <GameDetaiPage /> },

7aafd58575179ef968666dd2cfd0c2940dd37a17
Emiliya Dobrogorska <emiliyadobrogorska@Emiliyas-MacBook-Pro-2.local>
2024-03-27 19:54:46 -0700

    Refactor - Entities

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -6,14 +6,9 @@ import genres from "../data/genres";
 // import apiClient from "../services/api-client";
 import APIClient from "../services/api-client";
 import ms from "ms";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
-
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
 
 // interface FetchGenreResponse {
 //   count: number;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -3,14 +3,9 @@ import APIClient from "../services/api-client";
 // import apiClient from "../services/api-client";
 import platforms from "../data/platforms";
 import ms from "ms";
+import { Platform } from "../entities/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 
 // const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 
